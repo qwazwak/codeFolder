73c88e22e78fad141f55c5b548abaf0848ebebee
qwazwak <realqwazwak@gmail.com>
2018-03-20 11:02:44 -0500

    notes for program 3

---
+++ CSC-1300/program_3/BasicSort.cpp
@@ -0,0 +1,24 @@
+#include "BasicSort.h"
+
+using namespace std;
+
+void selectionSort (int array[], const int size)
+{
+	int min;
+	int swap;
+	
+	for (int i = 0; i < size; i++)
+	{
+		min = i;
+		for (int j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
+		swap = array[min];
+		array[min] = array[i];
+		array[i] = swap;	
+	}
+}	
