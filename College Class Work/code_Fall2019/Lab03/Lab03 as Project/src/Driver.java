2d965ddb5d6ec94a7a6340c12e9162f02f280125
Rus <realqwazwak@gmail.com>
2019-09-13 16:37:52 -0500

    might be complete

--- College Class Work/code_Fall2019/Lab03/Lab03 as Project/src/Driver.java
+++ College Class Work/code_Fall2019/Lab03/Lab03 as Project/src/Driver.java
@@ -9,7 +9,7 @@ public class Driver {
 		double runningTotal = 0;
 		IceCreamCone theFullCone;
 		long numberOfObjectsOrdered = 0;
-		String yes = new String("y");
+		final String no = new String("n");
 		String input;
 		boolean shouldContinue = true;
 		
@@ -24,16 +24,29 @@ public class Driver {
 			theFullCone = new IceCreamCone(new IceCream(theScoops, theFlavor, theTopping), theCone);
 			runningTotal += theFullCone.getPrice();
 			++numberOfObjectsOrdered;
-			
+
+			System.out.println("");
+			System.out.println("Your order:");
+			System.out.println("Cone:    " + theCone.toString());
+			System.out.println("Flavor:  " + theFlavor.toString());
+			System.out.println("Number of scoops: " + theScoops);
+			System.out.println("Topping: " + theTopping.toString());
+			System.out.println("Price:   " + theFullCone.getPrice());
+
+
+			System.out.println("");
 			input = cin.readString("Would you like to order an ice cream cone? (y/n)");
-			if(yes.compareTo(input) == 0) {
+			while(!(input.equals("y") || input.equals("n"))) {
+				System.out.println("Invalid input");
+				input = cin.readString("Would you like to order an ice cream cone? (y/n)");
+			}
+			if(no.equals(input)) {
 				shouldContinue = false;
 			}
 
 		}while(shouldContinue);
 		
-		System.out.println("Your total order for " + numberOfObjectsOrdered + " is $" + runningTotal);
-		System.out.println("ending");
+		System.out.println("Your total order for " + numberOfObjectsOrdered + "s is $" + runningTotal);
 	}
 	
 
@@ -42,11 +55,11 @@ public class Driver {
 
 		System.out.println(theRealToppings.listToppings());
 
-		theToppingID = cin.readInt("Enter the number of the topping you would like");
+		theToppingID = cin.readInt("Enter the number of the topping you would like: ");
 		while(theToppingID < 1 || theToppingID > theRealToppings.numToppings()) {
 			theRealToppings.listToppings();
 			System.out.println("Invalid input, try again");
-			theToppingID = cin.readInt("Enter the number of the topping you would like");
+			theToppingID = cin.readInt("Enter the number of the topping you would like: ");
 		}
 		
 		return theRealToppings.getTopping(theToppingID);
@@ -60,11 +73,11 @@ public class Driver {
 
 
 		
-		theFlavorID = cin.readInt("Enter the number of the flavor you would like");
+		theFlavorID = cin.readInt("Enter the number of the flavor you would like: ");
 		while(theFlavorID < 1 || theFlavorID > theRealFalvors.numFlavors()) {
 			theRealFalvors.listFlavors();
 			System.out.println("Invalid input, try again");
-			theFlavorID = cin.readInt("Enter the number of the flavor you would like");
+			theFlavorID = cin.readInt("Enter the number of the flavor you would like: ");
 		}
 		
 		return theRealFalvors.getFlavor(theFlavorID);
@@ -74,12 +87,12 @@ public class Driver {
 	public static int getScoopsChoice() {
 		int numScoops;
 	
-		numScoops = cin.readInt("How many scoops (1, 2, or 3) would you like?");
+		numScoops = cin.readInt("How many scoops (1, 2, or 3) would you like? ");
 		
 		while(numScoops < 1 || numScoops > 3)
 		{
 			System.out.println("Invalid input, try again");
-			numScoops = cin.readInt("How many scoops (1, 2, or 3) would you like?");
+			numScoops = cin.readInt("How many scoops (1, 2, or 3) would you like? ");
 		}
 		return numScoops;
 	}
@@ -90,13 +103,13 @@ public class Driver {
 		System.out.println("1. Sugar cone  - $0.59");
 		System.out.println("2. Waffle cone - $0.79");
 		System.out.println("3. Cup         - $0.00");
-		typeOfCone = cin.readInt("Enter the number of the cone you would like");
+		typeOfCone = cin.readInt("Enter the number of the cone you would like: ");
 		while(typeOfCone < 1 || typeOfCone > 3) {
 			System.out.println("1. Sugar cone  - $0.59");
 			System.out.println("2. Waffle cone - $0.79");
 			System.out.println("3. Cup         - $0.00");
 			System.out.println("Invalid input, try again");
-			typeOfCone = cin.readInt("Enter the number of the cone you would like");
+			typeOfCone = cin.readInt("Enter the number of the cone you would like: ");
 		}
 		return new Cone(typeOfCone);
 	}
