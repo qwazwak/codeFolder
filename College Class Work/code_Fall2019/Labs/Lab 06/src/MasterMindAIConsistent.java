3620e3b4dde7c9cfb9a4bccddea5e4e7e487aead
Rus <realqwazwak@gmail.com>
2019-10-03 12:04:25 -0700

    ye

--- College Class Work/code_Fall2019/Labs/Lab 06/src/MasterMindAIConsistent.java
+++ College Class Work/code_Fall2019/Labs/Lab 06/src/MasterMindAIConsistent.java
@@ -1,101 +1,96 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
-public class MasterMindAIConsistent
-{
-   private MasterMind game;
-
-   public MasterMindAIConsistent(MasterMind game)  
-   {
-      this.game = game;
-   }
-
-    //DO THIS
-   public Guess nextGuess(int guess_id)
-   {
-      List<Guess> guesses = game.getGuesses();
-      int num_guesses = guesses.size();
-      Guess nextGuess = null;
-
-      if (guesses.size() > 0)
-      {
-         Guess trialGuess = null;
-
-         boolean good = false;
-         //keep obtaining a random guess until you get one that works with all previous results
-         //it is cheating to keep obtaining a random guess until you match the solution
-		 //make a random guess and then analyze it
-
-
-
-
-
-
-
- 
-         nextGuess = trialGuess;  //found a good one
-      }
-      else
-      {
-         nextGuess = makeRandomGuess(guess_id);
-      }
-
-      return nextGuess;
-   }
-   
-   //DO THIS
-   //is the guess consistent with all previous results?
-   //that is, if you compare your random guess to a previous guess, do you get the same number of black and white buttons?
-   //don't just check your random guess against the secret guess until you get a good result
-   private boolean analyzeGuess(Guess nextGuess)
-   {
-      List<Guess> guesses = game.getGuesses();  //get all previous guesses from the game
-      int num_guesses = guesses.size();
-
-      //loop over all previous guesses
-      for (int i = 1; i < guesses.size(); i++) //note that i is starting at 1!
-      {
-		//previous guess compared to the secret guess (obtain the int array through the game ref)
+public class MasterMindAIConsistent implements MasterMindAI {
+	private MasterMind game;
+	
+	public MasterMindAIConsistent(MasterMind game) {
+		this.game = game;
+	}
+	
+	//DO THIS
+	public Guess nextGuess(int guess_id) {
+		List<Guess> guesses = game.getGuesses();
+		int num_guesses = guesses.size();
+		Guess nextGuess = null;
 		
+		if (guesses.size() > 0) {
+					boolean good = false;
+
+			Guess trialGuess = null;
+			
+			//	boolean good = false;
+			//keep obtaining a random guess until you get one that works with all previous results
+			//it is cheating to keep obtaining a random guess until you match the solution
+			//make a random guess and then analyze it
+			
+			while(!good) {
+				trialGuess = this.makeRandomGuess(guess_id);
+				good = this.analyzeGuess(trialGuess);
+				System.out.println("consistent 1st loop");
+			} 
+			
+			
+			
+			
+			
+			nextGuess = trialGuess; //found a good one
+		} else {
+			nextGuess = makeRandomGuess(guess_id);
+		}
 		
+		return nextGuess;
+	}
+	
+	//DO THIS
+	//is the guess consistent with all previous results?
+	//that is, if you compare your random guess to a previous guess, do you get the same number of black and white buttons?
+	//don't just check your random guess against the secret guess until you get a good result
+	private boolean analyzeGuess(Guess nextGuess) {
+		List<Guess> guesses = game.getGuesses(); //get all previous guesses from the game
+		int num_guesses = guesses.size();
 		
+		//loop over all previous guesses
+		for (int i = 1; i < guesses.size(); i++) //note that i is starting at 1!
+		{
+			//previous guess compared to the secret guess (obtain the int array through the game ref)
+			
+		//	int a = this.game.getResult(guesses.get(i - 1))[0];
+		//	int b = this.game.getResult(guesses.get(i - 1))[1];
+			Guess pastGuess = guesses.get(i - 1);
+			int[] pastResult = this.game.getResult(pastGuess);
+			
+			
+			//next guess is compared to previous guesses, NOT the secret guess
+			
+			int[] compOfNextGuess = this.game.getResult(pastGuess, nextGuess);
+			
+			//int c = this.game.getResult(nextGuess, guesses.get(i - 1))[0];
+			//int d = this.game.getResult(nextGuess, guesses.get(i - 1))[1];
+			
+			
+			//if our previous number of black buttons is not equal to the next number black buttons
+			//or if our previous number of white buttons is not equal to the next number of white buttons return false
+			if (pastResult[0] != compOfNextGuess[0] || pastResult[1] != compOfNextGuess[1]) {
+				return false;
+			}
+		}
 		
-
-		//next guess is compared to previous guesses, NOT the secret guess
-
-		
-		
-
-		//if our previous number of black buttons is not equal to the next number black buttons
-		//or if our previous number of white buttons is not equal to the next number of white buttons return false
-		if ( 							)
-         {
-            return false;
-         }
-      }
-
-      return true;
-   }
-
-  
- 
-   //DO THIS
-   //use the Random class to make a randomly generated guess
-   private static Guess makeRandomGuess(int guess_id)
-   {
-      Guess randomGuess = new Guess(guess_id);
-      Random random = Random.getRandomNumberGenerator();
-
-
-
-
-
-
-
-
-
-
-
-      return randomGuess;
-   }
+		return true;
+	}
+	
+	
+	
+	//DO THIS
+	//use the Random class to make a randomly generated guess
+	private static Guess makeRandomGuess(int guess_id) {
+		Guess randomGuess = new Guess(guess_id);
+		Random rand = new Random();
+		randomGuess.addGuess(1 + ( rand.nextInt(7)));
+		randomGuess.addGuess(1 + ( rand.nextInt(7)));
+		randomGuess.addGuess(1 + ( rand.nextInt(7)));
+		randomGuess.addGuess(1 + ( rand.nextInt(7)));
+		return randomGuess;
+	}
 }
