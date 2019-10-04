320790b27fc5032eebde9a314bc95285b7799b70
Rus <realqwazwak@gmail.com>
2019-10-04 10:42:10 -0700

    Update MasterMindAI.java

--- College Class Work/code_Fall2019/Labs/Lab 06/src/MasterMindAI.java
+++ College Class Work/code_Fall2019/Labs/Lab 06/src/MasterMindAI.java
@@ -1,7 +1,5 @@
+//Lab 06, Mastermind, by Rustan Hoffman and Rebecca Damewood
 
-//DO THIS
-//take a look at MasterMindAIRandom and decide what ONE method should be placed in this interface
-//this method will provide the interface between the game and any AIs that are written for the game
 public interface MasterMindAI {
 	public Guess nextGuess(int guess_id);
 }
